7c905c44e43cb235f4e2283560cd79d055efcbae
MrBrownLHS <jbrown@littletonschools.org>
2025-04-21 19:24:35 -0400

    auto

--- src/main/java/frc/robot/commands/AutoCenterStart.java
+++ src/main/java/frc/robot/commands/AutoCenterStart.java
@@ -8,12 +8,20 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.ArmLift;
+import frc.robot.subsystems.ArmPivot;
+import frc.robot.subsystems.ArmRotate;
+import frc.robot.subsystems.ArmIntake;
+import frc.robot.commands.MoveArmToL3Command;
 
 public class AutoCenterStart extends SequentialCommandGroup {
-  public AutoCenterStart(SwerveSubsystem swerve){ // CollectorArm collectorArm, CoralCollector coralCollector) {
+  public AutoCenterStart(
+    SwerveSubsystem swerve, ArmLift lift, ArmPivot pivot, ArmRotate rotate, ArmIntake intake){ // CollectorArm collectorArm, CoralCollector coralCollector) {
     addCommands(
       swerve.swerveAuto(-2.0, 0.0, 0.5, 0.0),  // Move forward 2m
       swerve.swerveAuto(0.0, 0, 0, 0.25),   // Move right 1.5m and rotate slowly
-      swerve.stopSwerveCommand());
+      swerve.stopSwerveCommand(),
+      new MoveArmToL3Command(lift, pivot, rotate, intake);
+      );
   }
 }
